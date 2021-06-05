68cfc491f9682d3a245c1a568fad3349e06c1025
lucaargolo <lucaargolo@gmail.com>
2021-06-05 17:08:26 -0300

    fix duplication recipe and shield decoration recipe working with entity banners

---
+++ src/main/java/io/github/lucaargolo/entitybanners/mixin/BannerDuplicateRecipeMixin.java
@@ -0,0 +1,28 @@
+package io.github.lucaargolo.entitybanners.mixin;
+
+import io.github.lucaargolo.entitybanners.EntityBanners;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.BannerDuplicateRecipe;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BannerDuplicateRecipe.class)
+public class BannerDuplicateRecipeMixin {
+
+    @Inject(at = @At("RETURN"), method = "matches", cancellable = true)
+    public void matches(CraftingInventory craftingInventory, World world, CallbackInfoReturnable<Boolean> cir) {
+        if(cir.getReturnValue()) {
+            for(int i = 0; i < craftingInventory.size(); ++i) {
+                ItemStack stack = craftingInventory.getStack(i);
+                if(stack.getItem() == EntityBanners.INSTANCE.getENTITY_BANNER_ITEM()) {
+                    cir.setReturnValue(false);
+                }
+            }
+        }
+    }
+
+}
